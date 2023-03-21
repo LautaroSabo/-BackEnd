4e1253fb3a1f5d695ce7fada9d8a064a71a61ff4
Lautaro Sabo <Lautysabo6@gmail.com>
2023-03-21 12:34:43 -0300

    Trabajo Finalizado

--- Clase 3/DesafioEntregable.js
+++ Clase 3/DesafioEntregable.js
@@ -22,26 +22,43 @@ class ProductManager{
         }
         if(!producto.title ||!producto.description ||!producto.thumbnail ||!producto.price ||!producto.code ||!producto.stock){
             return console.log("Debe Ingresar Toda la Información")
-        }if(code === this.productos.code){
-            return console.log(`El código usado ya existe ${producto.code}`)
-        }else return console.log("Se subió el producto ",this.productos.push(producto))
+        }
 
+        let getCode = this.productos.find((prod)=> prod.code==producto.code)
+        //console.log(getCode)
+        if(getCode){
+            return 'El codigo ya existe'
+        }else{
+            this.productos.push(producto)
+            return this.productos
+        }
+        
     }
 
     
     
-    getProductById(){
-
+    getProductById(filtroId){
+        let getId = this.productos.filter(Id =>Id.id === filtroId ).map(Id => Id.id)
+        //console.log(getId)
+        if(getId== 0){
+            return 'Not Found'
+        }
+        if (getId == filtroId ){
+            return `Se ha Encontrado el Id ${getId}  `
+            
+        }else{
+            return 'Not Found'
+        }
     }
 }
 
 const productManager = new ProductManager()
 
-let Productoss = productManager.addproduct("Arroz","Descripcion","foto",30,001,10);
-let Productoss2 = productManager.addproduct("Arroz2","Descripcion2","foto2",302,002,12)
-let Productoss3 = productManager.addproduct("Manteca","Descripcion2","foto2",15,003,40)
-let Productoss4 = productManager.addproduct("Manteca","Descripcion2","foto2",15,003,40)
+productManager.addproduct("Arroz","Descripcion","foto",30,001,5);
+productManager.addproduct("Tomate","Descripcion2","foto2",15,002,12)
+productManager.addproduct("Manteca","Descripcion3","foto3",15,003,40)
+productManager.addproduct("Agua","Descripcion4","foto4",15,003,40) //Producto con CODE Repetido
 
-console.log(Productoss,Productoss2,Productoss3,Productoss4)
-console.log(productManager.getProduct())
-//console.log(this.productos)
+//console.log(Productoss,Productoss2,Productoss3,Productoss4)
+//console.log(Productoss)
+console.log(productManager.getProductById(1))
